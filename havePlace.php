3cf2ba57f2a1d6d1192d29cf107663c47c851ffd
@arcturusInk <baruaswati1995@gmail.com>
2017-04-11 04:07:23 -0400

    starting the UI for 'Have A Place'

---
+++ havePlace.php
@@ -0,0 +1,14 @@
+<!doctype html>
+	<html class="no-js" lang="en" dir="ltr">
+		<head>
+			<meta charset="utf-8">
+			<meta http-equiv="x-ua-compatible" content="ie=edge">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Profile: I Have A Place</title>
+			<link rel="stylesheet" href="css/bootstrap.css">
+			<link rel="stylesheet" href="css/havePlace-style.css">
+		</head>
+	<body>
+	etc
+	</body>
+</html>
