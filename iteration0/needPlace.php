06d77e32fc76f4a24235fb013f4c16bff412a743
@arcturusInk <baruaswati1995@gmail.com>
2017-04-14 00:24:42 -0400

    first iteration of needPlace UI

---
+++ iteration0/needPlace.php
@@ -0,0 +1,239 @@
+<!doctype html>
+	<html class="no-js" lang="en" dir="ltr">
+		<head>
+			<meta charset="utf-8">
+			<meta http-equiv="x-ua-compatible" content="ie=edge">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Profile: I Need A Place</title>
+			<link rel="stylesheet" href="css/bootstrap.css">
+			<link rel="stylesheet" href="css/needPlace-style.css">
+		</head>
+	<body>
+
+		<div id="mainContainer">
+			<form class="form-horizontal">
+
+			<div class = "boundary">
+				<header>
+					<h3 id="">Location Preferences<h3>
+				</header>
+				<div class="form-group">
+					<label for="" class="col-sm-2 control-label">Within: </label>
+					<div class="col-sm-10">
+						<select class="form-control" id="" name="">
+						    <option value="">5 Mins</option>
+						    <option value="">10 Mins</option>
+						    <option value="">15 Mins</option>
+						    <option value="">20 Mins</option>
+						    <option value="">30 Mins</option>
+						    <option value="">1 Hour</option>
+						    <option value="">1 Hour 30 Mins</option>
+						</select> 
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="" class="col-sm-2 control-label">Of </label>
+					<div class="col-sm-10">
+						<select class="form-control" id="" name="">
+						    <option value="">Poly</option>
+						    <option value="">Washington Square Campus</option>
+						</select> 
+					</div>
+				</div>	
+				</div>
+
+				<div class = "boundary">
+				<header>
+					<h3 id="">Space Preferences<h3>
+				</header>				
+				<div class="form-group">
+					<label for="" class="col-sm-2 control-label">Maximun Rent (You Are Willing To Pay): </label>
+					<div class="col-sm-10">
+						<select class="form-control" id="" name="">
+						    <option value="">$2000+/Month</option>
+						    <option value="">$1500/Month</option>
+						    <option value="">$1000/Month</option>
+						    <option value="">$900/Month</option>
+						    <option value="">$800/Month</option>
+						    <option value="">$700/Month</option>
+						    <option value="">$600/Month</option>
+						    <option value="">$500/Month</option>
+						    <option value="">$400/Month</option>
+						    <option value="">$300/Month</option>
+						</select> 
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="" class="col-sm-2 control-label">Term For Leasing: </label>
+					<div class="col-sm-10">
+						<select class="form-control" id="" name="">
+						    <option value="">12 Month Or Less</option>
+						    <option value="">3 Month Or Less</option>
+						    <option value="">6 Month Or Less</option>
+						    <option value="">9 Month Or Less</option>
+						    <option value="">Month To Month</option>
+						</select> 
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="" class="col-sm-2 control-label">Bedroom Occupancy: </label>
+					<div class="col-sm-10">
+						<select class="form-control" id="" name="">
+						    <option value="">I Will Share A Bedroom</option>
+						    <option value="">I'd Like My Own Bedroom</option>
+						</select> 
+					</div>
+				</div>
+				</div>
+
+				<div class = "boundary">
+				<header>
+					<h3 id="">Roommate Preferences<h3>
+				</header>				
+				<div class="form-group">
+					<label for="" class="col-sm-2 control-label">Degree: </label>
+					<div class="col-sm-10">
+						<select class="form-control" id="" name="">
+						    <option value="">Undergrad</option>
+						    <option value="">Grad</option>
+						</select> 
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="" class="col-sm-2 control-label">Gender: </label>
+					<div class="col-sm-10">
+						<select class="form-control" id="" name="">
+						    <option value="">Female</option>
+						    <option value="">Male</option>
+						</select> 
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="" class="col-sm-2 control-label">Boundaries: </label>
+					<div class="col-sm-10">
+						<select class="form-control" id="" name="">
+						    <option value="">Likes To Share</option>
+						    <option value="">Likes To Be Alone</option>
+						</select> 
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="" class="col-sm-2 control-label">Prefers To Stay: </label>
+					<div class="col-sm-10">
+						<select class="form-control" id="" name="">
+						    <option value="">Out Late At Night</option>
+						    <option value="">In Home At Night</option>
+						</select> 
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="" class="col-sm-2 control-label">Sleeping Habits: </label>
+					<div class="col-sm-10">
+						<select class="form-control" id="" name="">
+						    <option value="">Sleeps Late (After 12)</option>
+						    <option value="">Sleeps Early (Before 12)</option>
+						</select> 
+					</div>
+				</div>
+				<p>For The Following, Select What Is Acceptable</p>
+				<div class="form-group">
+					<div class="checkbox">
+						<label for="" class="col-sm-2 control-label" id="textBold">Cleanliness: </label>
+						<div class="col-sm-10">
+						    <label> <input type="checkbox" name="cleanliness"> Clean </label>
+						    <label> <input type="checkbox" name="cleanliness"> Avg </label>
+						    <label> <input type="checkbox" name="cleanliness"> Messy </label> 
+						</div>
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="checkbox">
+						<label for="" class="col-sm-2 control-label" id="textBold" >Likes To: </label>
+						<div class="col-sm-10">
+						    <label> <input type="checkbox" name="likes"> Party </label>
+						    <label> <input type="checkbox" name="likes"> Drink </label>
+						</div>
+					</div>
+				</div>
+				</div>
+
+				<div class = "boundary">
+				<header>
+					<h3 id="">About Me<h3>
+				</header>
+				<div class="form-group">
+					<label for="" class="col-sm-2 control-label">Degree: </label>
+					<div class="col-sm-10">
+						<select class="form-control" id="" name="">
+						    <option value="">Undergrad</option>
+						    <option value="">Grad</option>
+						</select> 
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="" class="col-sm-2 control-label">Gender: </label>
+					<div class="col-sm-10">
+						<select class="form-control" id="" name="">
+						    <option value="">Female</option>
+						    <option value="">Male</option>
+						</select> 
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="" class="col-sm-2 control-label">Boundaries: </label>
+					<div class="col-sm-10">
+						<select class="form-control" id="" name="">
+						    <option value="">Like To Share</option>
+						    <option value="">Like To Be Alone</option>
+						</select> 
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="" class="col-sm-2 control-label">Prefer To Stay: </label>
+					<div class="col-sm-10">
+						<select class="form-control" id="" name="">
+						    <option value="">Out Late At Night</option>
+						    <option value="">In Home At Night</option>
+						</select> 
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="" class="col-sm-2 control-label">Sleeping Habits: </label>
+					<div class="col-sm-10">
+						<select class="form-control" id="" name="">
+						    <option value="">Sleep Late (After 12)</option>
+						    <option value="">Sleep Early (Before 12)</option>
+						</select> 
+					</div>
+				</div>
+				<p>For The Following, Select What Describes You</p>
+				<div class="form-group">
+					<div class="checkbox">
+						<label for="" class="col-sm-2 control-label" id="textBold">Cleanliness: </label>
+						<div class="col-sm-10">
+						    <label> <input type="checkbox" name="cleanliness"> Clean </label>
+						    <label> <input type="checkbox" name="cleanliness"> Avg </label>
+						    <label> <input type="checkbox" name="cleanliness"> Messy </label> 
+						</div>
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="checkbox">
+						<label for="" class="col-sm-2 control-label" id="textBold">Like To: </label>
+						<div class="col-sm-10">
+						    <label> <input type="checkbox" name="likes"> Party </label>
+						    <label> <input type="checkbox" name="likes"> Drink </label>
+						</div>
+					</div>
+				</div>
+				</div>
+
+				<div class="form-group">
+		    		<div class="col-sm-offset-2 col-sm-10">
+		      			<button type="submit" class="btn btn-info">Submit!</button>
+		    		</div>
+				</div>
+			</form>
+		</div>
+	</body>
+</html>
